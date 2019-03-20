169be029a1f3a6df57b8beef7da652ce97373716
Wong Jun Wei <junwei.wong@mail.utoronto.ca>
2019-03-20 17:55:54 -0400

    ready for milestone 1

--- EnigmaMachine.v
+++ EnigmaMachine.v
@@ -1,13 +1,13 @@
 `include "rotor.v"
 `include "reflector.v"
 
-module EnigmaMachine(SW, LEDR, KEY, CLOCK_50);
+module EnigmaMachine(SW, HEX0, HEX1, KEY, CLOCK_50);
     input [9:0] SW;
 	 input [3:0] KEY;
 	 input CLOCK_50;
-	 output [9:0] LEDR;
+	 output [6:0] HEX0, HEX1;
 	 wire [25:0] cov_out, rotor_out, ref_out, rotor_out2, cov_out2;
-	 reg [4:0] to_ledr;
+	 reg [4:0] to_hex;
 	 
 	 binary_to_alphabet b0(.in(SW[4:0]), .out(cov_out));
 	 rotor r0(.in(cov_out), .out(rotor_out), .clock(CLOCK_50), .rotate(~KEY[1]), .reset(~KEY[0]));
@@ -17,9 +17,18 @@ module EnigmaMachine(SW, LEDR, KEY, CLOCK_50);
 	 
 	 always @(posedge ~KEY[2])
 	     begin
-		      to_ledr <= cov_out2; 	    
+		      to_hex <= cov_out2; 	    
 		  end
-    assign	LEDR[4:0] = to_ledr;
+    
+	 hex_decoder H0(
+        .hex_digit(to_hex[3:0]), 
+        .segments(HEX0)
+        );
+        
+    hex_decoder H1(
+        .hex_digit({3'b000, to_hex[4]}), 
+        .segments(HEX1)
+        );
 endmodule
 
 module binary_to_alphabet(in, out);
@@ -96,4 +105,30 @@ module alphabet_to_binary(in, out);
 					 default: out = 5'b00000;
 				endcase
 		  end
+endmodule
+
+module hex_decoder(hex_digit, segments);
+    input [3:0] hex_digit;
+    output reg [6:0] segments;
+   
+    always @(*)
+        case (hex_digit)
+            4'h0: segments = 7'b100_0000;
+            4'h1: segments = 7'b111_1001;
+            4'h2: segments = 7'b010_0100;
+            4'h3: segments = 7'b011_0000;
+            4'h4: segments = 7'b001_1001;
+            4'h5: segments = 7'b001_0010;
+            4'h6: segments = 7'b000_0010;
+            4'h7: segments = 7'b111_1000;
+            4'h8: segments = 7'b000_0000;
+            4'h9: segments = 7'b001_1000;
+            4'hA: segments = 7'b000_1000;
+            4'hB: segments = 7'b000_0011;
+            4'hC: segments = 7'b100_0110;
+            4'hD: segments = 7'b010_0001;
+            4'hE: segments = 7'b000_0110;
+            4'hF: segments = 7'b000_1110;   
+            default: segments = 7'h7f;
+        endcase
 endmodule
